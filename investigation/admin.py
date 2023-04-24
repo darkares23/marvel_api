7714827a0ed4e89a2eba21631c799a8979301d30
Juan <desarrollojuseca@gmail.com>
2023-04-24 18:11:50 -0500

    added admin views for Character tables

--- investigation/admin.py
+++ investigation/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
-# Register your models here.
+from .models import Character
+
+
+class CharacterAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "description", "thumbnail", "thumbnail_preview")
+    search_fields = ("name",)
+
+    def thumbnail_preview(self, obj):
+        return format_html('<img src="{}" width="50" height="50" />', obj.thumbnail)
+
+    thumbnail_preview.short_description = "Thumbnail"
+
+
+admin.site.register(Character, CharacterAdmin)
